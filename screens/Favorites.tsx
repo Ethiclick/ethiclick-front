e893297ea3fec25a76290ae76b09b35a7a3f716c
Fabien Winkler <fabien.winkler@outlook.fr>
2023-11-02 19:16:12 +0100

    feat: create fav / notif view

---
+++ screens/Favorites.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Favorites() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Favorites</Text>
+    </View>
+  );
+}
