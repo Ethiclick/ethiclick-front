a01f5856988d36aa58d879c9d0fd80f5447535de
Fabien Winkler <fabien.winkler@outlook.fr>
2023-11-04 17:40:25 +0100

    feat: add menu user to search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
-import { Searchbar } from 'react-native-paper';
+import { Pressable, View } from 'react-native';
+import { Avatar, Button, Divider, Icon, Menu, PaperProvider, Searchbar } from 'react-native-paper';
 
 function SearchBar() {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [visible, setVisible] = React.useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
 
   const onChangeSearch = (query: string) => setSearchQuery(query);
 
+  const MenuButton = (
+    <Button onPress={openMenu}>
+      Show menu <Avatar.Text size={35} label="FW" />
+    </Button>
+  );
+
   return (
-    <Searchbar
-      elevation={2}
+    <View
       style={{
         position: 'absolute',
         top: 50,
@@ -17,10 +28,37 @@ function SearchBar() {
         backgroundColor: 'white',
         borderRadius: 15,
       }}
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+    >
+      <Searchbar
+        elevation={2}
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 15,
+        }}
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <View style={{ flex: 1, position: 'absolute', right: 5, top: 7, bottom: 0 }}>
+        <View style={{ alignItems: 'flex-end' }}>
+          <Menu
+            anchorPosition='bottom'
+            visible={visible}
+            onDismiss={closeMenu}
+            // contentStyle={{backgroundColor: COLORS.controlNormal}}
+            anchor={
+              <Button icon={'chevron-down'} style={{ padding: '0.75%' }} compact={true} onPress={() => openMenu()}>
+                {/* <Icon source={'chevron-down'} size={20} /> */}
+                <Avatar.Text size={30} label="XD" />
+              </Button>
+            }
+          >
+            <Menu.Item onPress={() => {}} title="My account" leadingIcon={'account-circle-outline'} />
+            <Menu.Item onPress={() => {}} title="Log out" leadingIcon={'logout'} />
+          </Menu>
+        </View>
+      </View>
+    </View>
   );
 }
 
