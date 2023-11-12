a634b7ae0e2199d6d1349dfe24423ce943930b0c
Fabien Winkler <fabien.winkler@outlook.fr>
2023-11-12 14:35:50 +0100

    fix: order of import

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 // import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import BottomNavigationBar from './components/BottomNavigationBar';
 import { Provider } from 'react-native-paper';
+import BottomNavigationBar from './components/BottomNavigationBar';
 
 export default function App() {
   return (
