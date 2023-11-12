9254d9556a5d410595aa002d0a374ae4ff7e6b7e
Fabien Winkler <fabien.winkler@outlook.fr>
2023-11-12 14:36:09 +0100

    fix: add portal for bottomnavbar

--- components/BottomNavigationBar.tsx
+++ components/BottomNavigationBar.tsx
@@ -1,35 +1,38 @@
+/* eslint-disable react/no-unstable-nested-components */
 import * as React from 'react';
 
-import { Icon } from 'react-native-paper';
+import { Icon, Portal } from 'react-native-paper';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
-import About from '../screens/About';
-import Favorites from '../screens/Favorites';
+// import About from '../screens/About';
+// import Favorites from '../screens/Favorites';
 import Notifications from '../screens/Notifications';
 import Profil from '../screens/Profil';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import Favorites from '../screens/Favorites';
 
 const Tab = createBottomTabNavigator();
 function BottomNavigationBar() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          // headerShown: false, // Globally
-          tabBarStyle: { position: 'absolute', bottom: 30, left: 15, right: 15, borderRadius: 12, opacity: 0.8 },
-        }}
-      >
-        <Tab.Screen
-          name="Accueil"
-          options={{
-            headerShown: false,
-            tabBarLabel: 'Accueil',
-            tabBarIcon: ({ color, size }) => <Icon source="home" color={color} size={size} />,
+    <Portal>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            // headerShown: false, // Globally
+            tabBarStyle: { position: 'absolute', bottom: 30, left: 15, right: 15, borderRadius: 12, opacity: 0.9 },
           }}
-          component={Home}
-        />
-        {/* <Tab.Screen
+        >
+          <Tab.Screen
+            name="Accueil"
+            options={{
+              headerShown: false,
+              tabBarLabel: 'Accueil',
+              tabBarIcon: ({ color, size }) => <Icon source="home" color={color} size={size} />,
+            }}
+            component={Home}
+          />
+          {/* <Tab.Screen
           options={{
             tabBarLabel: 'Rechercher',
             tabBarIcon: ({ color, size }) => <Icon source="magnify" color={color} size={size} />,
@@ -37,25 +40,34 @@ function BottomNavigationBar() {
           name="Rechercher"
           component={Favorites}
         /> */}
-        <Tab.Screen
-          options={{
-            // headerShown: true, // Specific view
-            tabBarLabel: 'Favoris',
-            tabBarIcon: ({ color, size }) => <Icon source="heart" color={color} size={size} />,
-          }}
-          name="Favoris"
-          component={Notifications}
-        />
-        <Tab.Screen
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ color, size }) => <Icon source="account" color={color} size={size} />,
-          }}
-          name="Profile"
-          component={Profil}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen
+            options={{
+              // headerShown: true, // Specific view
+              tabBarLabel: 'Favoris',
+              tabBarIcon: ({ color, size }) => <Icon source="heart" color={color} size={size} />,
+            }}
+            name="Favoris"
+            component={Favorites}
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: 'Notifications',
+              tabBarIcon: ({ color, size }) => <Icon source="bell" color={color} size={size} />,
+            }}
+            name="Notifications"
+            component={Notifications}
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color, size }) => <Icon source="account" color={color} size={size} />,
+            }}
+            name="Profile"
+            component={Profil}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Portal>
   );
 }
 
