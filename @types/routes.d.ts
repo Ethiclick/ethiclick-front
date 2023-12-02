1301843594f21b540b6bdac3ab3ea7c709221097
Fabien Winkler <fabien.winkler@outlook.fr>
2023-12-02 15:08:23 +0100

    chore: ajout types

---
+++ @types/routes.d.ts
@@ -0,0 +1,24 @@
+enum Routes {
+  Home = 'home',
+  Login = 'login',
+  Profile = 'profile',
+  Settings = 'settings',
+  Favorites = 'favorites',
+  Notifications = 'notifications',
+}
+
+export type NavigationParams = RootStackParams;
+
+export type RootStackParams = {
+  [Routes.Home]: undefined;
+  [Routes.Login]: undefined;
+};
+
+export type ProfileStackParams = {
+  [Routes.Profile]: undefined;
+  [Routes.Settings]: undefined;
+  [Routes.Favorites]: { id: string; username: string };
+  [Routes.Notifications]: { id: string; username: string };
+};
+
+export default Routes;
---
+++ @types/toast.d.ts
@@ -0,0 +1,12 @@
+export interface IToastType {
+  msg: string;
+  duration?: number;
+  type?: ToastColorEnum;
+}
+
+export enum ToastColorEnum {
+  Error = '#F44336',
+  Succes = '#4CAF50',
+  Warning = '#FF9800',
+  Info = '#2196F3',
+}
