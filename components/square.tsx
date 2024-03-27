08d6779d18f4db64987233760048b6bf7d13d5a7
EmmanuelleWinkler <111369621+EmmanuelleWinkler@users.noreply.github.com>
2024-03-27 18:06:06 +0100

    feat: ajout des card pour l'affichage list

--- components/square.tsx
+++ components/square.tsx
@@ -3,14 +3,7 @@ import { Card, Icon } from 'react-native-paper';
 import { View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    containerList: {
-      marginTop: 30,
-      backgroundColor: 'black',
-      flex: 1,
-      width: '100%',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-    },
+
     rowList: {
       width: '100%',
       flexDirection: 'row',
@@ -20,21 +13,22 @@ const styles = StyleSheet.create({
     },
     cardList: {
         borderRadius: 20,
+        flex: 1,
     }
   });
 
 const Square = ({ data1, data2 }) => (
     <View style={styles.rowList}>
         <Card.Title
-                  style={ [styles.cardList, { backgroundColor: data1.bgColor }]}
-                  title={data1.label}
-                  subtitle={data1.label}
-                  left={() => <Icon size={30} source="pasta" />}
+            style={ [styles.cardList, { backgroundColor: data1.bgColor }]}
+            title={data1.label}
+            // subtitle={data1.label}
+            left={() => <Icon size={30} source="pasta" />}
         />
         <Card.Title
             style={ [styles.cardList, { backgroundColor: data2.bgColor }]}
             title={data2.label}
-            subtitle={data2.label}
+            // subtitle={data2.label}
             left={() => <Icon size={30} source="pasta" />}
         />
     </View>
