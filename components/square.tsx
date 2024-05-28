66c1a6ac3e1461f2ca8da9e28fe5548e34ae5c3e
fabienwnklr <fabien.winkler@outlook.fr>
2024-05-28 17:34:52 +0200

    fix: add container style

--- components/square.tsx
+++ components/square.tsx
@@ -35,33 +35,35 @@ function Col({ children }: { children: React.JSX.Element }) {
 
 function Square({ data1, data2 }: { data1: { libelle: string; color: string }; data2: { libelle: string; color: string } }) {
   return (
-    <Row>
-      <Col>
-        <Button
-          style={styles.btn}
-          labelStyle={styles.title}
-          buttonColor={data1.color}
-          mode="contained"
-          contentStyle={{ height: '100%' }}
-          onPress={() => console.log(`${data1.libelle} pressed`)}
-        >
-          {data1.libelle}
-        </Button>
-      </Col>
+    <View style={styles.container}>
+      <Row>
+        <Col>
+          <Button
+            style={styles.btn}
+            labelStyle={styles.title}
+            buttonColor={data1.color}
+            mode="contained"
+            contentStyle={{ height: '100%' }}
+            onPress={() => console.log(`${data1.libelle} pressed`)}
+          >
+            {data1.libelle}
+          </Button>
+        </Col>
 
-      <Col>
-        <Button
-          style={styles.btn}
-          labelStyle={styles.title}
-          buttonColor={data2.color}
-          mode="contained"
-          contentStyle={{ height: '100%' }}
-          onPress={() => console.log(`${data2.libelle} pressed`)}
-        >
-          {data2.libelle}
-        </Button>
-      </Col>
-    </Row>
+        <Col>
+          <Button
+            style={styles.btn}
+            labelStyle={styles.title}
+            buttonColor={data2.color}
+            mode="contained"
+            contentStyle={{ height: '100%' }}
+            onPress={() => console.log(`${data2.libelle} pressed`)}
+          >
+            {data2.libelle}
+          </Button>
+        </Col>
+      </Row>
+    </View>
   );
 }
 
